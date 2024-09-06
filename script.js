8473b59ef1f86fe2c1afc3a5d23ee46e31f0e244
kazuki.yoshida <kazuki.yoshida@zozo.com>
2024-09-06 19:46:02 +0900

    1st commit

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', init);
+
+function init() {
+    
+}
+
+function shuffle() {
+    document.getElementById('thema1_text').innerHTML = 'テーマ1';
+    document.getElementById('thema2_text').innerHTML = 'テーマ2';
+    document.getElementById('thema3_text').innerHTML = 'テーマ3';
+}
+
